0a956f18d4f7345f4ef21a63e3132ef2c47bfb97
Mark Collis <mark.collis2001@gmail.com>
2022-08-05 20:06:35 +0100

    - Added the IO stream header - Output the sum of two numbers to the console

--- BookstoreProject/Main.cpp
+++ BookstoreProject/Main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+
 int main()
 {
-	return -1;
+	std::cout << "Enter two numbers: " << std::endl;
+	int Number1 = 0, Number2 = 0;
+	std::cin >> Number1 >> Number2;
+	std::cout << "The sum of " << Number1 << " and " << Number2 << " is " << Number1 + Number2 << std::endl;
+
+	return 0;
 }
